8e772f8a8f53fbf827bf1235d576a9898ded6f65
Jason Gersztyn <jgersztyn91@hotmail.com>
2024-01-28 02:16:10 -0700

    Creating a calculator app using .NET and Blazor

---
+++ Calculator.Core/Pages/CalculatorComponent.razor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Calculator.Core.Pages
+{
+    public partial class CalculatorComponent : ComponentBase
+    {
+        protected string currentResult;
+
+        private string firstValue;
+        private bool waitingForSecondValue;
+        private string operand;
+
+        public CalculatorComponent()
+        {
+            currentResult = "0";
+            firstValue = "";
+            waitingForSecondValue = false;
+            operand = null;
+        }
+
+        protected void EnterDigit(string digit)
+        {
+            if (currentResult == "0")
+            {
+                currentResult = "";
+            }
+
+            currentResult = string.Concat(currentResult, digit);
+        }
+
+        protected void EnterDecimal()
+        {
+            if (!currentResult.Contains("."))
+            {
+                currentResult = string.Concat(currentResult, ".");
+            }
+        }
+
+        protected void HandleOperand(string selectedOperator)
+        {
+            firstValue = currentResult;
+            operand = selectedOperator;
+
+            if (!waitingForSecondValue)
+            {
+                currentResult = "0";
+            }
+
+            waitingForSecondValue = true;
+        }
+
+        protected void Clear()
+        {
+            currentResult = "0";
+            firstValue = "";
+            waitingForSecondValue = false;
+            operand = null;
+        }
+
+        protected void Calculate()
+        {
+            try
+            {
+                switch(operand)
+                {
+                    case "+":
+                        Add();
+                        break;
+                    case "-":
+                        Subtract();
+                        break;
+                    case "*":
+                        Multiply();
+                        break;
+                    case "/":
+                        Divide();
+                        break;
+                    default:
+                        Clear();
+                        break;
+                }
+            }
+            catch
+            {
+                currentResult = "Error, Press AC to Continue";
+            }
+            finally
+            {
+                waitingForSecondValue = false;
+            }
+        }
+
+        private void Add()
+        {
+            currentResult = (Convert.ToDouble(firstValue) + Convert.ToDouble(currentResult)).ToString();
+        }
+
+        private void Subtract()
+        {
+            currentResult = (Convert.ToDouble(firstValue) - Convert.ToDouble(currentResult)).ToString();
+        }
+
+        private void Multiply()
+        {
+            currentResult = (Convert.ToDouble(firstValue) * Convert.ToDouble(currentResult)).ToString();
+        }
+
+        private void Divide()
+        {
+            if (Convert.ToDouble(currentResult) != 0)
+            {
+                currentResult = (Convert.ToDouble(firstValue) / Convert.ToDouble(currentResult)).ToString();
+            }
+
+            else
+            {
+                currentResult = "Cannot Divide by Zero";
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator Blazor App
+
+A calculator application capable of performing basic arithmitic operations.
+
+Tools used to build the calculator include .NET C# for the backend code, Blazor for the UI and bUnit for testing purposes.
